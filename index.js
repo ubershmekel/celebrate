57bb1d395d3fb2d44b1c989ad81ae614415e77c4
Yuval Greenfield <ubershmekel@gmail.com>
2022-01-13 20:52:07 -0800

    Longer fadeout, shuffle sounds

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ function realisticConfetti() {
     },
     angle: Math.random() * 20 + 80,
     resize: true,
+    ticks: 600, // time to fade
   };
 
   function fire(particleRatio, opts) {
@@ -46,29 +47,26 @@ function celebrate() {
 }
 window.celebrate = celebrate;
 
+
+const files = ['audience-claps.mp3', 'dudes-clap-yay.mp3', 'gasp-yay.mp3', 'kids-yay.mp3', 'one-yay.mp3'];
+let soundsIndex = 0;
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
 function playSound() {
-  const files = ['audience-claps.mp3', 'dudes-clap-yay.mp3', 'gasp-yay.mp3', 'kids-yay.mp3', 'one-yay.mp3'];
+  if (soundsIndex >= files.length) {
+    shuffleArray(files);
+    soundsIndex = 0;
+  }
   const filePath = files[Math.floor(Math.random() * files.length)];
+  soundsIndex++;
   var audio = new Audio('sounds/' + filePath);
+  audio.playbackRate = 0.8 + 0.4 * Math.random(); // 0.8 - 1.2 speed
   audio.play();
 }
 
-
-function changeFavicon(text) {
-  const canvas = document.createElement('canvas');
-  canvas.height = 64;
-  canvas.width = 64;
-  const ctx = canvas.getContext('2d');
-  ctx.font = '64px serif';
-  ctx.fillText(text, 0, 64);
-
-  const link = document.createElement('link');
-  const oldLinks = document.querySelectorAll('link[rel="shortcut icon"]');
-  oldLinks.forEach(e => e.parentNode.removeChild(e));
-  link.id = 'dynamic-favicon';
-  link.rel = 'shortcut icon';
-  link.href = canvas.toDataURL();
-  document.head.appendChild(link);
-}
-
-changeFavicon('🎉');
